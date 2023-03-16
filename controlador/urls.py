c2ed3253aed9abfb16caad0018a08145bd0bb6fd
SantiMike <198n0053@itstb.edu.mx>
2023-03-16 14:38:55 -0600

    Se crea el proyecto

---
+++ controlador/urls.py
@@ -0,0 +1,44 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='twitter/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('delete/<int:post_id>/', views.delete, name='delete'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('editar/', views.editar, name='editar'),
+    path('follow/<str:username>/', views.follow, name='follow'),
+    path('unfollow/<str:username>/', views.unfollow, name='unfollow'),
+
+    path('posts.json',       views.posts, name='posts'),
+    path('like/<int:id>',    views.like, name='like'),
+
+    path('ruta5', views.vista5, name='vista5'),
+    path('politicas', views.politicas, name='politicas'),
+    path('ayuda', views.ayuda, name='ayuda'),
+    path('comment', views.comment, name='comment'),
+    path('administrativas/titulacion', views.titulacion, name='titulacion'),
+    path('administrativas/residencias', views.residencias, name='residencias'),
+    path('administrativas/actcomplementarias', views.actcomplementarias, name='actcomplementarias'),
+    path('administrativas/servicios', views.servicios, name='servicios'),
+    path('administrativas/creditos', views.creditos, name='creditos'),
+    path('eventos/diasfestivos', views.diasfestivos, name='diasfestivos'),
+    path('eventos/aniversario', views.aniversario, name='aniversario'),
+    path('eventos/conferencias', views.conferencias, name='conferencias'),
+    path('eventos/eventosdep', views.eventosdep, name='eventosdep'),
+    path('noticias/convocatorias', views.convocatorias, name='convocatorias'),
+    path('noticias/comunicados', views.comunicados, name='comunicados'),
+
+
+
+
+
+
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
